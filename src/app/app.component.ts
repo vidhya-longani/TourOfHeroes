ec79ff33243249ac97e24b33e44b2787aaf472c4
vidhya-longani <vidhya.longani@kiwitech.com>
2017-06-19 17:35:40 +0530

    first country module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CountryModule } from './country/country.module';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1>
+  			<h2>{{country.name}} details!</h2>
+  			<div><label>id: </label>{{country.id}}</div>
+			<div><label>name: </label>{{country.name}}</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'World Tour';
+  country : CountryModule = {
+  	id : 1,
+  	name : 'India'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule
